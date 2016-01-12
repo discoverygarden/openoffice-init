badabb6bc32daa13c6b756eec77c1d4dc6c5f684
lutaylor <luke@discoverygarden.ca>
2016-01-12 15:37:57 -0400

    adding cleanup script and changing init script to use openoffice user

---
+++ secure_apache_openoffice.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+APACHE_USER=www-data
+#note use this is to clean up the old way of using apache to start openoffice since it is insecure.
+if [ -f /etc/init.d/openoffice ]; then
+	service openoffice stop
+	usermod --shell /bin/false $APACHE_USER
+	sed -i "s|$APACHE_USER|openoffice|g" /etc/init.d/openoffice
+	useradd -m -d /home/openoffice -s /bin/bash openoffice
+        service openoffice start
+fi
